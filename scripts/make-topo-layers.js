65bc40e3ee4e144718dd404d3b7db25213b15a43
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-10-01 10:27:33 +0100

    make sure old output layers are deleted before generating new ones

--- scripts/make-topo-layers.js
+++ scripts/make-topo-layers.js
@@ -1,19 +1,33 @@
 import fs from "fs";
+import { execSync } from "child_process";
 import mapshaper from "mapshaper";
 
 const keys = ["ltla", "utla", "cty", "mcty", "cauth", "rgn", "ctry", "uk"];
+const inDir = "./output/merge";
+const outDir = "./output/layers";
+
 const years = JSON.parse(
-  fs.readFileSync("./output/merge/years.json", {encoding: "utf8", flag: "r"})
+  fs.readFileSync(`${inDir}/years.json`, {encoding: "utf8", flag: "r"})
 );
 const lookup = JSON.parse(
-  fs.readFileSync("./output/merge/lookup.json", {encoding: "utf8", flag: "r"})
+  fs.readFileSync(`${inDir}/lookup.json`, {encoding: "utf8", flag: "r"})
 );
 
+// Clear output directory
+try {
+  execSync(`rm -r ${outDir}/*`);
+  console.log(`Emptied folder ${outDir}`);
+}
+catch {
+  console.log(`No files to delete in ${outDir}`);
+}
+
+// Generate GeoJSON for each year of each geography
 const layers = [];
 keys.forEach(key => {
   years.forEach(y => {
     if (lookup.find(l => l[`${key}${y}cd`])) {
-      mapshaper.runCommands(`-i ./output/merge/ltla-merge.json -rename-fields areacd=${key}${y}cd,areanm=${key}${y}nm -filter 'areacd !== undefined' -dissolve2 areacd,areanm name=${key}${y} -clean -o ./output/layers/${key}${y}.json format=geojson`);
+      mapshaper.runCommands(`-i ${inDir}/ltla-merge.json -rename-fields areacd=${key}${y}cd,areanm=${key}${y}nm -filter 'areacd !== undefined' -dissolve2 areacd,areanm name=${key}${y} -clean -o ${outDir}/${key}${y}.json format=geojson`);
       layers.push(`${key}${y}`);
     }
   });
